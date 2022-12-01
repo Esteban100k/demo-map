a76dbfc0b98de56eba46b17e6d519ac3370d4a07
Esteban Londoño Ospina <stebanlondo75@gmail.com>
2022-12-01 09:36:11 -0500

    Center Map

--- src/map/MapChart.tsx
+++ src/map/MapChart.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useMemo } from "react";
+import React, { useEffect, useState, useMemo, Fragment } from "react";
 import { ComposableMap, Geographies, Geography } from "react-simple-maps";
 import { csv } from "d3-fetch";
 import { scaleLinear } from "d3-scale";
@@ -6,6 +6,10 @@ import sortBy from "lodash/sortBy";
 import features from './features.json';
 import dataCountry from './dataCountry.json';
 import CircleMarker from './CircleMarker';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
 
 const MapChart = () => {
     const [data, setData] = useState([]);
@@ -17,30 +21,41 @@ const MapChart = () => {
         setData(sortedCities);
     }, []);
 
-    const popScale = useMemo(() => scaleLinear().domain([0, maxValue]).range([0, 24]),[maxValue]);
+    const popScale = useMemo(() => scaleLinear().domain([0, maxValue]).range([0, 24]), [maxValue]);
 
     return (
-        <ComposableMap>
-            <Geographies geography={features}>
-                {({ geographies }) =>
-                    geographies.map((geo) => (
-                        <Geography key={geo.rsmKey} geography={geo} fill="#41547C" />
-                    ))
-                }
-            </Geographies>
-
-            {data.map(({ city_code, lng, lat, population, country }) => {
-                return (
-                    <CircleMarker
-                        country={country}
-                        city_code={city_code}
-                        lng={lng}
-                        lat={lat}
-                        rValue={popScale(population)}
-                    />
-                );
-            })}
-        </ComposableMap>
+        <Fragment>
+            <div style={{ position:'relative', right:'50px' }}>
+            <Grid container spacing={5}>
+                <Grid item xs={12} md={2}>
+                </Grid>
+                <Grid item xs={12} md={8}>
+                    <ComposableMap>
+                        <Geographies geography={features}>
+                            {({ geographies }) =>
+                                geographies.map((geo) => (
+                                    <Geography key={geo.rsmKey} geography={geo} fill="#41547C" />
+                                ))
+                            }
+                        </Geographies>
+                        {data.map(({ city_code, lng, lat, population, country }) => {
+                            return (
+                                <CircleMarker
+                                    country={country}
+                                    city_code={city_code}
+                                    lng={lng}
+                                    lat={lat}
+                                    rValue={popScale(population)}
+                                />
+                            );
+                        })}
+                    </ComposableMap>
+                </Grid>
+                <Grid item xs={12} md={2}></Grid>
+            </Grid>
+            </div>
+           
+        </Fragment>
     );
 };
 
